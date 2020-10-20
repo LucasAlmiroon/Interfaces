098da2e09db58b85413ff41abdc5c4b1f54ea49b
lucasalmi <almiron736@gmail.com>
2020-10-20 12:50:11 -0300

    Inputs y botones con efecto, carga de pagina.

--- TP3/TPE-3/sites/js/script.js
+++ TP3/TPE-3/sites/js/script.js
@@ -1,34 +1,19 @@
+function cargarPagina(){
+    let elementos = document.getElementsByClassName("esconder");
+    while (elementos.length > 0){
+        elementos[0].classList.remove("esconder");
+    }
 
-function countdown (){
-    let hoy = new Date();
-    let estreno = new Date(2020,11,7);
-
-    let hora = hoy.getTime();
-    let horaEstreno = estreno.getTime();
-
-    let resta = horaEstreno - hora;
-
-    let s = Math.floor(resta / 1000);
-    let m = Math.floor(s/60);
-    let h = Math.floor(m/60);
-    let d = Math.floor(h/24);
-
-    h %= 24;
-    m %= 60;
-    s %= 60;
-
-    h = (h < 10) ? "0" + h: h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-
-
-    document.querySelector("#dias").textContent = d;
-    document.querySelector("#dias").innerHTML = d;
-
-    document.querySelector("#horas").textContent = h;
-    document.querySelector("#minutos").textContent = m;
-    document.querySelector("#segundos").textContent = s;
-
+    let carga = document.getElementsByClassName("carga");
+    carga[0].classList.add("esconder");
+    let body = document.querySelector("body");
+    body.classList.add("animacion");
 }
-countdown();
-setInterval(countdown,1000);
+
+function quitarAnimacion(){
+    let body = document.getElementsByClassName("animacion");
+    body[0].classList.remove("animacion");
+}
+
+setTimeout(cargarPagina,3000);
+setTimeout(quitarAnimacion,6000);
