98cdf267470b2e1af0322445c964c4ec749c21ed
lucasalmi <almiron736@gmail.com>
2021-10-07 11:59:36 -0300

    Mejore la interfaz y puse timer.

--- TPE-2/docs/sites/js/main.js
+++ TPE-2/docs/sites/js/main.js
@@ -12,6 +12,7 @@ let jugador2;
 let cliqueado;
 let turno = true;
 let ganador = 0;
+let timer = 300;
 
 //Crea la matriz con la cantidad de columnas y filas.
 function crearMatriz(){    
@@ -24,15 +25,21 @@ function crearMatriz(){
 }
 
 function CrearJugadores(){
-    jugador1 = new Jugador(document.querySelector("#nombre1").value,document.querySelector("#colorF1").value,true,false,20,20);
-    
-    jugador2 = new Jugador(document.querySelector("#nombre2").value,document.querySelector("#colorF2").value,false,false,20,850);
+    if(document.querySelector("#nombre1").value != "" && document.querySelector("#nombre2").value != ""){
+        jugador1 = new Jugador(document.querySelector("#nombre1").value,document.querySelector("#colorF1").value,true,false,20,20);
+        jugador2 = new Jugador(document.querySelector("#nombre2").value,document.querySelector("#colorF2").value,false,false,20,850);
 
-    juegoNuevo();
+        juegoNuevo();
+    }else{
+        alert("Ingrese nombre de jugadores");
+    }
+    
+
 }
 //Se crea un juego nuevo, por ahora solo crea las fichas.
 function juegoNuevo(){
     crearMatriz();
+    document.querySelector("#turno").innerHTML = "Turno jugador: " + jugador1.name;
     //Se crea el tablero, con una imagen de fondo.
     let imageTablero = new Image();
     imageTablero.src = "./sites/img/tablero.png";
@@ -49,6 +56,20 @@ function juegoNuevo(){
     }
     jugador1.generarFichas(ctx);
     jugador2.generarFichas(ctx);
+
+    document.querySelector("#botonPrincipal").style.margin = 0;
+    document.querySelector(".juegoNuevo").innerHTML = 'Reiniciar';
+
+    setInterval(function(){
+        timer--;
+        if(timer >= 1){
+            document.querySelector("#timer").innerHTML = 'Restan: ' + timer + ' segundos';
+        }else if(timer <= 0){
+            console.log("entra")
+            document.querySelector("#timer").innerHTML = 'Restan : 0 segundos';
+            chequearGanador();
+        }
+    },1000)
 }
 
 //Dibuja cada ficha del arreglo y el tablero.
@@ -170,20 +191,29 @@ function setY(a){
 }
 //Seteo los turnos y muestro en pantalla.  
 function setTurno(){
+    let body = document.querySelector("#turno");
     if (!jugador1.ganador && !jugador2.ganador){
         jugador1.setTurno();
         jugador2.setTurno();
     }
+    if(jugador1.turno){
+        body.innerHTML = "Turno jugador: " + jugador1.name;
+
+    }else{
+        body.innerHTML = "Turno jugador: " + jugador2.name; 
+    }
 }
 //Verifico si hay ganador y lo muestro en pantalla.
 function chequearGanador(){
-
+    let body = document.querySelector("#turno");
     if (jugador1.ganador){
-        console.log (jugador1.name);
+        body.innerHTML = "Ganador jugador: " + jugador1.name;
     }else if (jugador2.ganador){
-        console.log(jugador2.name);
-    }else{
-        //body.innerHTML = "Hay empate";
+        body.innerHTML = "Ganador jugador: " + jugador2.name;
+    }else if(timer <= 0){
+        body.innerHTML = "Se acabo el tiempo, empate!"
+        jugador1.turno = false;
+        jugador2.turno = false;
     }
 }
 
@@ -256,4 +286,4 @@ function onMouseUp(e){
   
   canvas.addEventListener('mousedown',onMouseDown,false);
 
-  document.querySelector('.juegoNuevo').addEventListener('click',CrearJugadores);
+  document.querySelector('.empezar').addEventListener('click',CrearJugadores);
